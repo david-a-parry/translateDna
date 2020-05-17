678271f26736d1e1c915d7197b7a60bdc7f93702
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-05-17 22:44:03 +0100

    Formatting

--- translateDna.pl
+++ translateDna.pl
@@ -4,23 +4,23 @@
 use warnings;
 use strict;
 use Getopt::Long;
-my %codons = 
+my %codons =
 (
-    AAA=>"K", AAC=>"N", AAG=>"K", AAT=>"N", 
-    ACA=>"T", ACC=>"T", ACG=>"T", ACT=>"T", 
-    AGA=>"R", AGC=>"S", AGG=>"R", AGT=>"S", 
-    ATA=>"I", ATC=>"I", ATG=>"M", ATT=>"I", 
-    CAA=>"Q", CAC=>"H", CAG=>"Q", CAT=>"H", 
-    CCA=>"P", CCC=>"P", CCG=>"P", CCT=>"P", 
-    CGA=>"R", CGC=>"R", CGG=>"R", CGT=>"R", 
-    CTA=>"L", CTC=>"L", CTG=>"L", CTT=>"L", 
-    GAA=>"E", GAC=>"D", GAG=>"E", GAT=>"D", 
-    GCA=>"A", GCC=>"A", GCG=>"A", GCT=>"A", 
-    GGA=>"G", GGC=>"G", GGG=>"G", GGT=>"G", 
-    GTA=>"V", GTC=>"V", GTG=>"V", GTT=>"V", 
-    TAA=>"*", TAC=>"Y", TAG=>"*", TAT=>"Y", 
-    TCA=>"S", TCC=>"S", TCG=>"S", TCT=>"S", 
-    TGA=>"*", TGC=>"C", TGG=>"W", TGT=>"C", 
+    AAA=>"K", AAC=>"N", AAG=>"K", AAT=>"N",
+    ACA=>"T", ACC=>"T", ACG=>"T", ACT=>"T",
+    AGA=>"R", AGC=>"S", AGG=>"R", AGT=>"S",
+    ATA=>"I", ATC=>"I", ATG=>"M", ATT=>"I",
+    CAA=>"Q", CAC=>"H", CAG=>"Q", CAT=>"H",
+    CCA=>"P", CCC=>"P", CCG=>"P", CCT=>"P",
+    CGA=>"R", CGC=>"R", CGG=>"R", CGT=>"R",
+    CTA=>"L", CTC=>"L", CTG=>"L", CTT=>"L",
+    GAA=>"E", GAC=>"D", GAG=>"E", GAT=>"D",
+    GCA=>"A", GCC=>"A", GCG=>"A", GCT=>"A",
+    GGA=>"G", GGC=>"G", GGG=>"G", GGT=>"G",
+    GTA=>"V", GTC=>"V", GTG=>"V", GTT=>"V",
+    TAA=>"*", TAC=>"Y", TAG=>"*", TAT=>"Y",
+    TCA=>"S", TCC=>"S", TCG=>"S", TCT=>"S",
+    TGA=>"*", TGC=>"C", TGG=>"W", TGT=>"C",
     TTA=>"L", TTC=>"F", TTG=>"L", TTT=>"F"
 );
 
@@ -35,7 +35,7 @@ GetOptions(
     'd|dna',            #output DNA as well
     's|stop',           #stop at first termination codon
     'h|?|help',
-) or usage("Syntax error.\n"); 
+) or usage("Syntax error.\n");
 
 usage() if $opts{h};
 
@@ -56,7 +56,7 @@ if (-f $opts{i} or -p $opts{i} or $opts{i} eq '-'){
     $opts{i} =~ s/['"]//g;
     if ($opts{i} =~ /^[\sacgtuACGTU]+$/){
         $opts{i} =~ s/\s+/\n/g;
-        open ($IN, "<", \$opts{i}) 
+        open ($IN, "<", \$opts{i})
             or die "Error getting filehandle from input string: $!\n";
     }else{
         die "ERROR: '$opts{i}' does not exist as a file and does not look ".
@@ -64,25 +64,24 @@ if (-f $opts{i} or -p $opts{i} or $opts{i} eq '-'){
     }
 }
 
-my $OUT = \*STDOUT;#main output defaults to STDOUT
+my $OUT = \*STDOUT; # main output defaults to STDOUT
 if ($opts{o}){
     open ($OUT, ">", $opts{o}) or die "Can't open $opts{o} for writing: $!\n";
 }
 my $prev_header = '';
 my $dna = '';
 while (my $line = <$IN>){
-    chomp $line; 
+    chomp $line;
     if ($line =~ /^>(.*)/){
         my $header = $1;
         translateDna($dna, $prev_header) if $dna;
         $prev_header = $header;
         $dna = '';
     }else{
-        $dna .= $line; 
+        $dna .= $line;
     }
 }
 translateDna($dna, $prev_header) if $dna;
-
 
 #########################################################
 sub translateDna{
@@ -110,14 +109,14 @@ sub outputTranslation{
     }else{
         outputTranslationOnly($protein, $name);
     }
-    print $OUT "\n"; 
+    print $OUT "\n";
 }
 
 #########################################################
 sub outputTranslationOnly{
-    my ($protein, $name) = @_; 
+    my ($protein, $name) = @_;
     print $OUT ">$name\n" if ($name);
-    my $num_length = length(length($protein)); 
+    my $num_length = length(length($protein));
     for (my $i = 0; $i < length($protein); $i += $line_length){
         my $l = $line_length;
         if ($i + $line_length > length($protein)){
@@ -128,14 +127,15 @@ sub outputTranslationOnly{
             my $n = $i + 1; #protein position
             $p = sprintf("%${num_length}d: %s", $n, $p);
         }
-        print $OUT "$p\n"; 
+        print $OUT "$p\n";
     }
 }
+
 #########################################################
 sub outputDnaAndProtein{
     my ($dna, $protein, $name) = @_;
-    my $max_protein = length($protein); 
-    my $num_length = length(length($dna)); 
+    my $max_protein = length($protein);
+    my $num_length = length(length($dna));
     $protein = join('', map { "-$_-" } split('', $protein) );
     $protein = '-' x (abs($frame) -1)  . $protein;
     print $OUT ">$name\n\n" if ($name);
@@ -154,21 +154,20 @@ sub outputDnaAndProtein{
             $p = substr($protein, $i, $pl);
         }
         if ($opts{n}){
-            my $n = $i + 1; #dna position
+            my $n = $i + 1; # dna position
             $d = sprintf("%${num_length}d: %s", $n, $d);
-            my $cn = $i - (abs($frame) - 1); #0-based coding DNA position of first letter of line
-            my $pn = 1;#protein position... 
+            my $cn = $i - (abs($frame) - 1); # 0-based coding DNA position of first letter of line
+            my $pn = 1; # protein position...
             if ($i > 0){
                 $pn = int($cn/3) + 1 ;
-                $pn++ if ($cn % 3 == 2); #if first nucleotide of line is the last letter of 
-                                     #codon we represent the next protein 'letter'
+                $pn++ if ($cn % 3 == 2); # if first nucleotide of line is the last letter of
+                                         # codon we represent the next protein 'letter'
             }
             $pn = $pn <= $max_protein ? $pn : $max_protein;
             $p = sprintf("%${num_length}d: %s", $pn, $p);
-            
         }
         print "$d\n$p\n\n";
-    } 
+    }
 }
 
 #########################################################
@@ -179,7 +178,7 @@ sub getDnaInFrame{
     }else{
         $dna =~ tr/uU/tT/;
     }
-    my $f = abs($frame); 
+    my $f = abs($frame);
     return ($dna, substr($dna, $f - 1) );
 }
 
@@ -187,7 +186,7 @@ sub getDnaInFrame{
 sub revComp{
     my $dna = shift;
     $dna =~ tr/uU/tT/;
-    $dna =~ tr/acgtACGT/tgcaTGCA/; 
+    $dna =~ tr/acgtACGT/tgcaTGCA/;
     return reverse($dna);
 }
 
@@ -196,19 +195,19 @@ sub usage{
     my $msg = shift;
     print STDERR "ERROR: $msg\n" if $msg;
     print <<EOT
-    
+
     usage: $0 -i <dna_input.fa>  [options]
     usage: $0 -i ATGCCGCTACGC... [options]
 
     Options:
 
     -i, --input FILE/STRING
-        Either an input file containing a single DNA sequence to be 
+        Either an input file containing a single DNA sequence to be
         translated (FASTA header optional) or several FASTA formatted sequences
-        OR 
-        a single DNA sequence (containing only the standard DNA/RNA letters 
+        OR
+        a single DNA sequence (containing only the standard DNA/RNA letters
         and whitespace) to be translated. If your sequence contains any spaces
-        you must enclose it with quotes (e.g. "ATG CCC GGG"). 
+        you must enclose it with quotes (e.g. "ATG CCC GGG").
 
     -o, --ouptut FILE
         Output file. Optional. Default is STDOUT.
@@ -217,8 +216,8 @@ sub usage{
         Number of letters per line. Default = 60.
 
     -f, --frame INT
-        Frame of translation. Valid values are 1 to 3 for translation 
-        starting at the first to third letter of the DNA input or -1 to -3 
+        Frame of translation. Valid values are 1 to 3 for translation
+        starting at the first to third letter of the DNA input or -1 to -3
         for translation of the reverse complement. Default = 1.
 
     -n, --numbers
@@ -228,7 +227,7 @@ sub usage{
         Output DNA lined up with protein.
 
     -s, --stop
-        Stop translation at first termination codon. Default behaviour is 
+        Stop translation at first termination codon. Default behaviour is
         to translate all codons in input.
 
     -?, -h, --help
@@ -249,7 +248,7 @@ sub usage{
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
     MA  02110-1301, USA.
 
 EOT
